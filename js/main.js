c87a8544784729868eb624c5c2c62db38b752ba5
Kevin Roark <ker2143@columbia.edu>
2014-04-05 04:01:09 -0400

    near final stage -- maybe need more youtube comments

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ $(function() {
 
   var video = document.querySelector('#video');
   var $vid = $(video);
+  var vidReady = false;
+
+  var audio = document.querySelector('#audio');
+  var $aud = $(audio);
+  var audReady = false;
 
   var $img = $('<img class="labor-pain-image">');
   $img.attr('src', 'media/fb_pains.png');
   var img = $img.get(0);
+
+  var activeBrightness, activeColor, activeWarp, activeAngelique, activeComments;
 
   var WALK_IN = 18000;
   var START_JULIE = 21500;
@@ -23,29 +30,78 @@ $(function() {
   var PAIN2E = 146000;
   var PAIN3S = 155000;
   var PAIN3E = 173500;
-
+  var ENDVISTIME = 285000;
+  var AUDIO_LENGTH = 364000;
   var MAX_BRIGHT = 666;
 
   video.addEventListener("canplaythrough", function() {
+    vidReady = true;
+    if (audReady)
+      start();
+  });
 
+  audio.addEventListener("canplaythrough", function() {
+    audReady = true;
+    if (vidReady)
+      start();
+  });
+
+  function start() {
+    audio.play();
     video.play();
-    setTimeout(hideFooter, 500);
-    setTimeout(step1, 15000);
+
+    setTimeout(hideFooter, 1000);
+    setTimeout(step1, 17000);
     setTimeout(step2, 35000);
-    setTimeout(step3, 50000);
+    setTimeout(step3, 53000);
     setTimeout(step4, C2S - 1500);
     setTimeout(step5, PAIN1S + 1500);
     setTimeout(soundsFun, THAT_SOUNDS_FUN);
     setTimeout(julieMasters, START_JULIE);
     setTimeout(owies, PAIN1S + 2000);
+    setTimeout(cutTheCrap, ENDVISTIME);
+    setTimeout(endgame, AUDIO_LENGTH);
 
     soundControl();
     speedControl();
 
-    setInterval(function() {
-      $('.debug-timer').html(video.currentTime);
-    }, 200);
-  });
+    //setInterval(function() {
+    //  $('.debug-timer').html(audio.currentTime);
+    //}, 200);
+  }
+
+  function cutTheCrap() {
+    activeComments = false;
+  }
+
+  function endgame() {
+
+    function restart() {
+      activeBrightness = false;
+      activeColor = false;
+      activeWarp = false;
+      kt.clearTransforms($vid);
+      kt.clearFilters($vid);
+      MAX_BRIGHT = 666;
+
+      audio.currentTime = 0;
+      video.currentTime = 0;
+      start();
+    }
+
+    function showFooter() {
+      $('.footer').animate({
+        opacity: 1.0
+      }, 600);
+
+      $('.footer').unbind('mouseenter');
+      $('.footer').unbind('mouseleave');
+    }
+
+    showFooter();
+    activeAngelique = false;
+    setTimeout(restart, 5000);
+  }
 
   function hideFooter() {
     $('.footer').animate({
@@ -74,27 +130,7 @@ $(function() {
       video.volume = num;
     }
 
-    var toggleTimes = [
-      WALK_IN, START_JULIE, END_JULIE,
-      C1S, C1E, C2S, C2E,
-      PAIN1S, PAIN1E, PAIN2S, PAIN2E, PAIN3S, PAIN3E
-    ];
-
-    setInterval(function() {
-      var t = toggleTimes[0];
-      var c = video.currentTime * 1000;
-      var diff = Math.abs(c - t);
-      if (diff < 100) { // a toggle time
-        toggle();
-        toggleTimes.shift(); // remove this time
-        if (t == C1E) { // cut volume here
-          vol(0.5);
-        } else if (t == C2E) { // and more here
-          vol(0.3);
-        }
-      }
-    }, 150);
-
+    video.muted = true;
   }
 
   function speedControl() {
@@ -106,13 +142,16 @@ $(function() {
       PAIN1S, PAIN1E, PAIN2S, PAIN2E, PAIN3S, PAIN3E
     ];
 
-    setInterval(function() {
+    var timer = setInterval(function() {
       var t = speedTimes[0];
       var c = video.currentTime * 1000;
       var diff = Math.abs(c - t);
       if (diff < 100) { // a speed time
         setSpeed(t);
         speedTimes.shift(); // remove this one
+        if (speedTimes.length == 0) {
+          clearInterval(timer);
+        }
       }
     }, 150);
 
@@ -129,7 +168,7 @@ $(function() {
         case PAIN3S:
           speed(0.2); break;
         case PAIN3E:
-          speed(1.0); break;
+          speed(0.9); break;
         default:
           break;
       }
@@ -144,9 +183,11 @@ $(function() {
       kt.brightness($vid, b);
 
       var next = kt.randInt(250, 50);
-      setTimeout(brightnessShift, next);
+      if (activeBrightness)
+        setTimeout(brightnessShift, next);
     }
 
+    activeBrightness = true;
     brightnessShift();
   }
 
@@ -166,39 +207,38 @@ $(function() {
       kt.invert($vid, inv);
 
       var next = kt.randInt(350, 60);
-      setTimeout(colorShift, next);
+      if (activeColor)
+        setTimeout(colorShift, next);
     }
 
     MAX_BRIGHT = 400;
+    activeColor = true;
     colorShift();
   }
 
   function step5() {
     function transform() {
-      var scale = kt.randInt(6, 1);
       var deg = kt.randInt(360);
       var xs = kt.randInt(60) - 30; // -30 -> 30
       var ys = kt.randInt(60) - 30; // -30 -> 30
 
-      kt.scale($vid, scale);
       kt.rotate($vid, deg);
       kt.skew($vid, xs, ys);
 
-      console.log('transforming');
-
-      var clear = kt.randInt(1500, 500);
+      var clear = kt.randInt(1000, 500);
       setTimeout(clearTransforms, clear);
     }
 
     function clearTransforms() {
-      kt.scale($vid, 1);
       kt.rotate($vid, 0);
       kt.skew($vid, 0, 0);
 
-      var next = kt.randInt(6000, 3000);
-      setTimeout(transform, next);
+      var next = kt.randInt(5000, 1500);
+      if (activeWarp)
+        setTimeout(transform, next);
     }
 
+    activeWarp = true;
     transform();
   }
 
@@ -240,7 +280,11 @@ $(function() {
           movement = 'r';
       }
 
-      setTimeout(getAngeliqueGoing, 30);
+      if (activeAngelique)
+        setTimeout(getAngeliqueGoing, 30);
+      else {
+        $img.css('width', '0px');
+      }
     }
 
     $('body').append($img);
@@ -250,12 +294,11 @@ $(function() {
     var movement = 'l';
     var aX = 20;
     var aY = 20;
+    activeAngelique = true;
     getAngeliqueGoing();
-
   }
 
   function step3() {
-
     var numComments = 7;
 
     function flashAComment() {
@@ -272,9 +315,11 @@ $(function() {
       $('body').append(c);
 
       removeLater(c);
-      setTimeout(flashAComment, kt.randInt(10000, 2000));
+      if (activeComments)
+        setTimeout(flashAComment, kt.randInt(10000, 2000));
     }
 
+    activeComments = true;
     flashAComment();
   }
 
@@ -323,9 +368,8 @@ $(function() {
     }
 
     showOw();
-    setTimeout(showOw, 10000);
-    setTimeout(showOw, 35000);
-    setTimeout(showOw, 55000);
+    setTimeout(showOw, 15000);
+    setTimeout(showOw, 51000);
   }
 
 });
